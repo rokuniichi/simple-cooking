fac8248e9aecb6b8158b1fbe4e24a64bba40087a
nightryder-cmd <cornypleb@gmail.com>
2022-04-05 13:01:46 +0700

    Level timer

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    MainConfigManager _mainConfigManager;
+    
+    MainConfig _config => _mainConfigManager.MainConfig;
+    
+    public float LevelTime { get; private set; }
+
+    void Awake() {
+        if ( Instance ) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        Setup();
+    }
+
+    void Start()
+    {
+        LevelTime = _config.LevelTime;
+    }
+
+    void Update()
+    {
+        LevelTime -= Time.deltaTime;
+        if (LevelTime < 0)
+        {
+            LevelTime = 0f;
+            Time.timeScale = 0f;
+        }
+    }
+
+    void Setup()
+    {
+        _mainConfigManager = new MainConfigManager();
+    }
+}
---
+++ Assets/Scripts/UI/TopUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TopUI : MonoBehaviour
+{
+    public TMP_Text Timer;
+    public TMP_Text CustomersNumber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var timerText = TimeSpan.FromSeconds(GameController.Instance.LevelTime).ToString("mm':'ss");
+        if ( timerText != Timer.text ) {
+            Timer.text = timerText;
+        }
+    }
+}
